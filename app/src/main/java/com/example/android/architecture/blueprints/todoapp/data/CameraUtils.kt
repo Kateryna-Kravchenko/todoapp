ac9f2aeb52beb85b3ace926b253500405e1d08a7
Kateryna <kravchenko_kateryna@ukr.net>
2022-05-17 12:42:07 +0300

    Update CameraUtils.kt

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/CameraUtils.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/CameraUtils.kt
@@ -1,209 +1,103 @@
-package com.example.android.architecture.blueprints.todoapp.data
 
 import android.content.Context
-import android.content.res.Resources
-import android.graphics.*
-import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import com.example.android.architecture.blueprints.todoapp.R
-import java.io.ByteArrayOutputStream
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import com.mysuperdispatch.android.extension.lazyFast
+import com.mysuperdispatch.android.utils.Utils
 import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStream
-import java.util.*
 
-fun getCircleBitmap(color: Int, size: Int, activityContext: Context): Drawable {
-    val a = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888)
-    val canvas = Canvas(a)
-    val paint = Paint()
-    paint.isAntiAlias = true
-    paint.color = color
-    paint.style = Paint.Style.FILL
-    canvas.drawBitmap(a, 0f, 0f, paint)
-    canvas.drawCircle(size / 2f, size / 2f, size / 2f, paint)
-    return BitmapDrawable(activityContext.resources, a)
-}
+class ImageLoader {
 
- fun saveStreamToFile(inputStream: InputStream,
-                             resources: Resources,
-                             activity: Context): Pair<String?, String?> {
-    var outputPath: String? = null
-    var originalOutputPath: String? = null
-    try {
-        val filesDir = activity.getExternalFilesDir(null)
-        val photo: Bitmap?
-        val options = BitmapFactory.Options()
-        options.inMutable = true
-        options.inDensity = 5000
-        options.inScreenDensity = 5000
-        options.inTargetDensity = 5000
-
-        photo = BitmapFactory.decodeStream(inputStream, null, options)
-
-        inputStream.close()
-
-        val originImageFile = File(filesDir, System.currentTimeMillis().toString() + "_.jpg")
-        if (originImageFile.createNewFile()) {
-            val fos = FileOutputStream(originImageFile)
-            photo!!.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-            fos.close()
+  class Builder constructor(context: Context) {
+    var requestManager: RequestManager = Glide.with(context)
+    private lateinit var requestBuilder: RequestBuilder<Drawable>
+    lateinit var imageLoaderCallback: ImageLoadingCallback
+    private val glideCallback by lazyFast {
+      object : RequestListener<Drawable> {
+        override fun onLoadFailed(
+          e: GlideException?,
+          model: Any?,
+          target: Target<Drawable>?,
+          isFirstResource: Boolean
+        ): Boolean {
+          imageLoaderCallback.onFailed(e)
+          return false
         }
-        originalOutputPath = originImageFile.path
-        val logo = BitmapFactory.decodeResource(resources, R.drawable.ic_add)
 
-        //----------------------draw timestamp----------------
-        val cs = Canvas(photo)
-        val tPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-
-        //get timestamp text
-        val dateTime = Date().toString()
-
-        //calculating text size
-        val textSize = photo.width / 40f
-        tPaint.isAntiAlias = true
-        tPaint.isFilterBitmap = true
-        tPaint.color = Color.WHITE
-        tPaint.style = Paint.Style.FILL
-        tPaint.textSize = textSize
-
-        val footerHeight = 1.67f * textSize
-        //draw background rect
-        tPaint.color = Color.BLACK
-        tPaint.alpha = 75
-        cs.drawRect(0f, photo.height - footerHeight, photo.width.toFloat(), photo.height.toFloat(), tPaint)
-
-        //draw texts
-        tPaint.alpha = 100
-        tPaint.textSize = textSize
-        tPaint.color = Color.WHITE
-        tPaint.style = Paint.Style.FILL
-        cs.drawText(dateTime, 20f, photo.height - textSize / 2, tPaint)
-
-        val src = Rect(0, 0, logo.width, logo.height)
-
-        val horizontalPadding = 0.2f * footerHeight
-        val logoTargetHeight = 0.9f * footerHeight
-        val scale = logoTargetHeight / logo.height
-        val logoTargetWith = scale * logo.width
-
-        val right = photo.width - horizontalPadding
-        val left = right - logoTargetWith
-        val bottom = photo.height - (footerHeight - logoTargetHeight) / 2
-        val top = bottom - logoTargetHeight
-
-        val dest = RectF(left, top, right, bottom)
-        val logoPaint = Paint(Paint.FILTER_BITMAP_FLAG or Paint.ANTI_ALIAS_FLAG or Paint.DITHER_FLAG)
-        cs.drawBitmap(logo, src, dest, logoPaint)
-        val text = "TODO application"
-        val textWith = tPaint.measureText(text)
-        cs.drawText(text, dest.left - horizontalPadding - textWith, photo.height - textSize / 2, tPaint)
-
-        //-----------------compress bitmap------------------
-        val bytes = ByteArrayOutputStream()
-        photo.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-        bytes.close()
-        photo.recycle()
-
-        //--------------------save to file----------------------
-        val filePath = File(filesDir, System.currentTimeMillis().toString() + ".jpg").toString()
-        val f = File(filePath)
-        if (f.createNewFile()) {
-            val fo = FileOutputStream(f)
-            fo.write(bytes.toByteArray())
-            fo.close()
+        override fun onResourceReady(
+          resource: Drawable?,
+          model: Any?,
+          target: Target<Drawable>?,
+          dataSource: DataSource?,
+          isFirstResource: Boolean
+        ): Boolean {
+          imageLoaderCallback.onSuccess(resource)
+          return false
         }
-        outputPath = filePath
-
-    } catch (ignored: Throwable) {
+      }
     }
-    return Pair(originalOutputPath, outputPath)
+
+    private fun locaLoad(resourceId: Int): Builder {
+      requestBuilder = requestManager.load(resourceId)
+      return this
+    }
+
+    private fun locaLoad(file: File): Builder {
+      requestBuilder = requestManager.load(file)
+      return this
+    }
+
+    fun load(file: File): Builder {
+      return locaLoad(file)
+    }
+
+    fun load(resourceId: Int): Builder {
+      return locaLoad(resourceId)
+    }
+
+    fun load(url: String): Builder {
+      requestBuilder = requestManager.load(url)
+      return this
+    }
+
+    fun isNotTrimEmpty(text: CharSequence?): Boolean {
+      return !Utils.isTrimEmpty(text)
+    }
+
+    
+    
+    // todo
+    private fun error(resourceId: Int): Builder {
+      requestBuilder = requestBuilder.apply(RequestOptions.errorOf(resourceId))
+      return this
+    }
+
+    fun cache(isEnabled: Boolean): Builder {
+      requestBuilder = if (isEnabled) {
+        requestBuilder.diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+      } else {
+        requestBuilder.diskCacheStrategy(DiskCacheStrategy.NONE)
+      }
+      requestBuilder = requestBuilder.skipMemoryCache(!isEnabled)
+      return this
+    }
+
+    fun into(imageView: ImageView) {
+      requestBuilder.into(imageView)
+    }
+  }
 }
 
-suspend fun drawOnPhotoFile(photoFromCamera: File,
-                    resources: Resources,
-                    activity: Context): Pair<String?, String?> {
-    var outputPath: String? = null
-    var originalOutputPath: String? = null
-    try {
-        originalOutputPath = photoFromCamera.path
-        val options = BitmapFactory.Options()
-        options.inMutable = true
-        val photo: Bitmap? = BitmapFactory.decodeFile(originalOutputPath, options)
-        outputPath = photo?.let { drawLogoOnPhotoFile(it, resources, activity) }
-    } catch (ignored: Throwable) {
-    }
-    return Pair(originalOutputPath, outputPath)
+interface ImageLoadingCallback {
+  fun onSuccess(drawable: Drawable?)
+  fun onFailed(ex: Throwable?)
 }
-
-private fun drawLogoOnPhotoFile(photo: Bitmap, resources: Resources,
-                                activity: Context): String {
-    val logo = BitmapFactory.decodeResource(resources, R.drawable.ic_add)
-    val filesDir = activity.getExternalFilesDir(null)
-
-    //----------------------draw timestamp----------------
-    val cs = Canvas(photo)
-    val tPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-
-    //get timestamp text
-    val dateTime = Date().toString()
-
-
-    //calculating text size
-    val textSize = photo.width / 40f
-    tPaint.isAntiAlias = true
-    tPaint.isFilterBitmap = true
-    tPaint.color = Color.WHITE
-    tPaint.style = Paint.Style.FILL
-    tPaint.textSize = textSize
-
-    val footerHeight = 1.67f * textSize
-    //draw background rect
-    tPaint.color = Color.BLACK
-    tPaint.alpha = 75
-    cs.drawRect(0f, photo.height - footerHeight, photo.width.toFloat(), photo.height.toFloat(), tPaint)
-
-    //draw texts
-    tPaint.alpha = 100
-    tPaint.textSize = textSize
-    tPaint.color = Color.WHITE
-    tPaint.style = Paint.Style.FILL
-    cs.drawText(dateTime, 20f, photo.height - textSize / 2, tPaint)
-
-    val src = Rect(0, 0, logo.width, logo.height)
-
-    val horizontalPadding = 0.2f * footerHeight
-    val logoTargetHeight = 0.9f * footerHeight
-    val scale = logoTargetHeight / logo.height
-    val logoTargetWith = scale * logo.width
-
-    val right = photo.width - horizontalPadding
-    val left = right - logoTargetWith
-    val bottom = photo.height - (footerHeight - logoTargetHeight) / 2
-    val top = bottom - logoTargetHeight
-
-    val dest = RectF(left, top, right, bottom)
-    val logoPaint = Paint(Paint.FILTER_BITMAP_FLAG or Paint.ANTI_ALIAS_FLAG or Paint.DITHER_FLAG)
-    cs.drawBitmap(logo, src, dest, logoPaint)
-    val text = "TODO application"
-    val textWith = tPaint.measureText(text)
-    cs.drawText(text, dest.left - horizontalPadding - textWith, photo.height - textSize / 2, tPaint)
-
-    //-----------------compress bitmap------------------
-    val bytes = ByteArrayOutputStream()
-    photo.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-    bytes.close()
-    photo.recycle()
-
-    //--------------------save to file----------------------
-    val filePath = File(filesDir, System.currentTimeMillis().toString() + ".jpg").toString()
-    val f = File(filePath)
-    if (f.createNewFile()) {
-        val fo = FileOutputStream(f)
-        fo.write(bytes.toByteArray())
-        fo.close()
-    }
-    return filePath
-}
-
-
-
